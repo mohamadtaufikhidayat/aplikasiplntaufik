78d9935731ad85ef6feb46494c3a4e07bb71af99
Rahmi Nailatunisrina <rahminisrinanaila@gmail.com>
2022-04-29 12:39:45 +0700

    membuat get list topik berasdarkan kelompok topik

--- src/main/java/com/prodemy/pembayaran/listrik/Repository/TopikPengaduanrepo.java
+++ src/main/java/com/prodemy/pembayaran/listrik/Repository/TopikPengaduanrepo.java
@@ -3,5 +3,11 @@ package com.prodemy.pembayaran.listrik.Repository;
 import com.prodemy.pembayaran.listrik.model.entity.TopikPengaduan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface TopikPengaduanrepo extends JpaRepository<TopikPengaduan,Long> {
+//    Optional<TopikPengaduan> findByKelompokTopik(String n);
+        List<TopikPengaduan> findAllByKelompokTopik(String kelompokTopik);
 }
+
--- src/main/java/com/prodemy/pembayaran/listrik/controller/TopikPengaduancontroller.java
+++ src/main/java/com/prodemy/pembayaran/listrik/controller/TopikPengaduancontroller.java
@@ -4,11 +4,13 @@ import com.prodemy.pembayaran.listrik.Repository.TopikPengaduanrepo;
 import com.prodemy.pembayaran.listrik.model.dto.TopikPengaduanDto;
 import com.prodemy.pembayaran.listrik.model.entity.TopikPengaduan;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -21,31 +23,33 @@ public class TopikPengaduancontroller {
         this.topikPengaduanrepo = topikPengaduanrepo;
     }
 
+    @GetMapping("/list-topik-berdasarkan-kelompok/{kelompokTopik}")
+    public List<TopikPengaduanDto> getListTopikBerdasarkanKelompok(@PathVariable String kelompokTopik){
+        List<TopikPengaduanDto> list = new ArrayList<>();
+        for(TopikPengaduan x: topikPengaduanrepo.findAllByKelompokTopik(kelompokTopik)){
+            list.add(convertEntityToDto2(x));
+        }
+        return list;
+    }
 
     @GetMapping("/list-kelompok-topik")
-    public List<TopikPengaduanDto> getListKelompokTopik(){
+    public List<TopikPengaduanDto> getListPadam(){
         List<TopikPengaduanDto> list = new ArrayList<>();
         for(TopikPengaduan x: topikPengaduanrepo.findAll()){
             list.add(convertEntityToDto1(x));
         }
         return list;
     }
-    @GetMapping("/list-topik")
-    public List<TopikPengaduanDto> getListPadam(){
+    @GetMapping("/list-all-topik")
+    public List<TopikPengaduanDto> getListTopik(){
         List<TopikPengaduanDto> list = new ArrayList<>();
         for(TopikPengaduan x: topikPengaduanrepo.findAll()){
             list.add(convertEntityToDto2(x));
         }
         return list;
     }
-    public TopikPengaduan convertDtoToEntity (TopikPengaduanDto dto) {
-        TopikPengaduan entity = new TopikPengaduan();
-        entity.setKelompokTopik(dto.getKelompokTopik());
-        entity.setNamaTopik(dto.getNamaTopik());
-        return entity;
-    }
 
-    public TopikPengaduanDto convertEntityToDto1 (TopikPengaduan entity){
+     public TopikPengaduanDto convertEntityToDto1 (TopikPengaduan entity){
         TopikPengaduanDto dto = new TopikPengaduanDto();
         dto.setKelompokTopik(entity.getKelompokTopik());
         return dto;
@@ -54,5 +58,11 @@ public class TopikPengaduancontroller {
         TopikPengaduanDto dto = new TopikPengaduanDto();
         dto.setNamaTopik(entity.getNamaTopik());
         return dto;
+    }
+    public TopikPengaduan convertDtoToEntity (TopikPengaduanDto dto) {
+        TopikPengaduan entity = new TopikPengaduan();
+        entity.setKelompokTopik(dto.getKelompokTopik());
+        entity.setNamaTopik(dto.getNamaTopik());
+        return entity;
     }
 }
