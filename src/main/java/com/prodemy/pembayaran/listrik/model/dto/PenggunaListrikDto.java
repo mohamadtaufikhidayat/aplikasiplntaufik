ccc41b2a1bf72a1b5b37c0e5e3bdcc6ad30f5bca
Karohmalia <karohmatul.amalia@gmail.com>
2022-05-08 23:10:43 +0700

    yukbisayuk

--- src/main/java/com/prodemy/pembayaran/listrik/model/dto/PenggunaListrikDto.java
+++ src/main/java/com/prodemy/pembayaran/listrik/model/dto/PenggunaListrikDto.java
@@ -1,12 +1,17 @@
 package com.prodemy.pembayaran.listrik.model.dto;
 
+import javax.persistence.Column;
+
 public class PenggunaListrikDto {
     private Long idPengguna;
     private String namaPengguna;
     private String alamat;
+    private String provinsi;
+    private String kota;
+    private String kecamatan;
+    private String kelurahan;
     private String jenisPengguna;
-    private Long idJenis;
-
+    private Long user_id;
     private Long daya;
 
     public Long getIdPengguna() {
@@ -49,11 +54,43 @@ public class PenggunaListrikDto {
         this.daya = daya;
     }
 
-    public Long getIdJenis() {
-        return idJenis;
+    public Long getUser_id() {
+        return user_id;
     }
 
-    public void setIdJenis(Long idJenis) {
-        this.idJenis = idJenis;
+    public void setUser_id(Long user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getProvinsi() {
+        return provinsi;
+    }
+
+    public void setProvinsi(String provinsi) {
+        this.provinsi = provinsi;
+    }
+
+    public String getKota() {
+        return kota;
+    }
+
+    public void setKota(String kota) {
+        this.kota = kota;
+    }
+
+    public String getKecamatan() {
+        return kecamatan;
+    }
+
+    public void setKecamatan(String kecamatan) {
+        this.kecamatan = kecamatan;
+    }
+
+    public String getKelurahan() {
+        return kelurahan;
+    }
+
+    public void setKelurahan(String kelurahan) {
+        this.kelurahan = kelurahan;
     }
 }
